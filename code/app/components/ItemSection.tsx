9c1667a4388b0ec8f10a87b0745a5adff0e131f3
rehanmirza76 <abdulrehan0317@gmail.com>
2025-01-18 17:12:03 +0500

    update

--- code/app/components/ItemSection.tsx
+++ code/app/components/ItemSection.tsx
@@ -13,24 +13,28 @@ export default function ItemSection() {
           <p className="font-semibold text-lg">Title</p>
           <p>description</p>
           <p className="text-green-500">$500</p>
+          <div className="border-[2px] text-blue-400 border-blue-400 p-2 w-[6.5rem] rounded-lg cursor-pointer hover:bg-blue-400  hover:text-white transition-all active:bg-blue-500">Order Now</div>
         </div>
         <div className="flex flex-col space-y-2 card bg-gray-200 p-2 rounded-lg ">
           <img src="/dummy.png" alt="" />
           <p className="font-semibold text-lg">Title</p>
           <p>description</p>
           <p className="text-green-500">$500</p>
+           <div className="border-[2px] text-blue-400 border-blue-400 p-2 w-[6.5rem] rounded-lg cursor-pointer hover:bg-blue-400  hover:text-white transition-all active:bg-blue-500">Order Now</div>
         </div>
         <div className="flex flex-col space-y-2 card bg-gray-200 p-2 rounded-lg ">
           <img src="/dummy.png" alt="" />
           <p className="font-semibold text-lg">Title</p>
           <p>description</p>
           <p className="text-green-500">$500</p>
+           <div className="border-[2px] text-blue-400 border-blue-400 p-2 w-[6.5rem] rounded-lg cursor-pointer hover:bg-blue-400  hover:text-white transition-all active:bg-blue-500">Order Now</div>
         </div>
         <div className="flex flex-col space-y-2 card bg-gray-200 p-2 rounded-lg ">
           <img src="/dummy.png" alt="" />
           <p className="font-semibold text-lg">Title</p>
           <p>description</p>
           <p className="text-green-500">$500</p>
+           <div className="border-[2px] text-blue-400 border-blue-400 p-2 w-[6.5rem] rounded-lg cursor-pointer hover:bg-blue-400  hover:text-white transition-all active:bg-blue-500">Order Now</div>
         </div>
       </div>
     </div>
--- code/app/page.tsx
+++ code/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/app/components/Hero";
 import ItemSection from "./components/ItemSection";
+import BottomImg from "./components/BottomImg";
 
 export default function Home() {
   return (
    <div>
     <Hero/>
     <ItemSection/>
+    <BottomImg/>
    </div>
   );
 }
