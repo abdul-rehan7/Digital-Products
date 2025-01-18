cb2ec7833f00b0d7ebd69ba690c86e86bfcbf5f4
rehanmirza76 <abdulrehan0317@gmail.com>
2025-01-18 14:21:34 +0500

    update

---
+++ code/app/components/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import img from '../public/app/imgs/hero.jpg'
+
+export default function Hero() {
+  return (
+    <div>
+        I am hero
+      <img src="/hero.jpg" alt="Img here" />
+    </div>
+  )
+}
