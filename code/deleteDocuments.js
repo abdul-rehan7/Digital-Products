6ca7d3b2bd18fa12d66b9b4ddc49862e2b3bbe71
rehanmirza76 <abdulrehan0317@gmail.com>
2025-01-19 22:19:19 +0500

    update

--- code/deleteDocuments.js
+++ code/deleteDocuments.js
@@ -4,7 +4,7 @@ const client = createClient({
   projectId: 'auf2vsuj', // Replace with your project ID
   dataset: 'production',     // Replace with your dataset name
   useCdn: false,
-  token:"sk2b81QPrPzgQedyuop7nAdR9ffjSlj2GgUU8tC4FsQ9bz5nwgkbZ1gdFElnjpTsSlZ0BIFLc8qFCkwgWfXLndjmWoS6xvhOfjJSD23fV7FPwbu9pWS7x1WsdTbaw0N8wi55BDdlvc9bGPukvitakwtMkbe5vcAbDeETWG4NU22ngR9kA1Yj"              // Optional: Set to false for real-time data
+  token:process.env.NEW_SANITY_TOKEN              // Optional: Set to false for real-time data
 });
 
 const deleteDocuments = async () => {
