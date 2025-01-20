98491391741bc384d92308d5b4aad2392963994a
rehanmirza76 <abdulrehan0317@gmail.com>
2025-01-20 09:34:08 +0500

    update

--- code/deleteDocuments.js
+++ code/deleteDocuments.js
@@ -2,8 +2,9 @@ const { createClient } = require('@sanity/client');
 
 const client = createClient({
   projectId: 'auf2vsuj', // Replace with your project ID
-  dataset: 'production',     // Replace with your dataset name
+  dataset:  process.env.NEXT_PUBLIC_SANITY_DATASET,     // Replace with your dataset name
   useCdn: false,
+  token: process.env.SANITY_NEW_API,
       
 });
 
